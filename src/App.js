407f7f46dc5514e09bc5ae1a508747b11a228604
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-11-22 20:02:52 -0300

    💄 Estilizando a barra de navegação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   );
 };
 

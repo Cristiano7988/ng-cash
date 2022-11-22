9ab22256c7d97d2796121e70f7d7a90df876ebf9
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-11-22 02:42:45 -0300

    ✨ Adição da verificação do token de acesso no Frontend

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,9 +11,44 @@ import ProtectedLayout from "../componentes/ProtectedLayout";
 import HomeLayout from "../componentes/HomeLayout";
 import Cadastro from "../componentes/pages/Cadastro";
 
+function logout() {
+  localStorage.removeItem("user");
+  window.location.pathname = "/login";
+}
+
+// Testa validade do token de acesso
+const getUserData = () => {
+  new Promise((resolve) => {
+    const user = localStorage.getItem("user");
+    if (!user || ["undefined", "null"].includes(user)) return;
+    const { accessToken } = JSON.parse(user);
+
+    fetch("/api/test/user", {
+      headers: {
+        "x-access-token": accessToken,
+      }
+    })
+    .then((r) => {
+      if (r.status === 200) return r.json();
+      logout();
+    })
+    .then((r) => {
+      if (r.status) {
+        const user = localStorage.getItem("user");
+        return resolve(user);
+      } 
+      logout();
+    })
+    .catch(() => logout());
+  });
+};
+
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<AuthLayout />}>
+    <Route
+      element={<AuthLayout />}
+      loader={() => defer({ userPromise: getUserData() })}
+    >
       <Route element={<HomeLayout />}>
         <Route path="/" element={<>Home</>} />
         <Route path="/login" element={<Login />} />

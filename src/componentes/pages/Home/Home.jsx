8a1a1ca8319db565586efc99df86f2a28fec1b35
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-11-28 12:22:01 -0300

    📝 Adicionando informações sobre o projeto

--- src/componentes/pages/Home/Home.jsx
+++ src/componentes/pages/Home/Home.jsx
@@ -8,6 +8,10 @@ const Home = () => {
             component="h1"
             children="Home"
         />
+        <Typography
+            component="p"
+            children="Somos o app financeiro da Nova Geração! Uma plataforma tecnológica com tudo o que é necessário para dar início à uma vida financeira responsável e controlada. Nosso propósito é fazer com que a GenZ se torne a geração mais independente com relação ao seu dinheiro, estando preparada para enfrentar todo e qualquer desafio que venha a aparecer!"
+        />
     </section>
   )
 };
